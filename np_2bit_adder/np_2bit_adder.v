87517999a41831149e57a527c0950d6d22268101
NightSailor <63074164+Gabriel836@users.noreply.github.com>
2024-07-16 15:53:10 -0300

    Added np_2bit_adder files
    Added verilog code for non-parallel 2-bit adder

---
+++ np_2bit_adder/np_2bit_adder.v
@@ -0,0 +1,26 @@
+/* ----- ----- ----- ----- ----- -----
+	Design: non-parallel 2bit adder
+ 	Filename: np_2bit_adder.v
+	Coder: Gabriel Maruschi
+ 	Versions: /july_2024/
+	----- ----- ----- ----- ----- -----
+*/ 
+
+module np_2bit_adder (co, s, a, b);
+output [1:0]s;   // s: sum; co: carry out;
+output co;
+input [1:0] a, b;    // (a, b): 2-bit input; 
+
+// Expression parcels for s[1] were divided to clarify the logic
+wire aux0 = (a[1]) & (~ b[1]) & (~ b[0]);
+wire aux1 = (a[1]) & (~ a[0]) & (~ b[1]);
+wire aux2 = (~ a[1]) & (a[0]) & (~ b[1]) & (b[0]);
+wire aux3 = (a[1]) & (a[0]) & (b[1]) & (b[0]);
+wire aux4 = (~ a[1]) & (~ a[0]) & (b[1]);
+wire aux5 = (~ a[1]) & (b[1]) & (~ b[0]);
+assign s[1] = aux0 | aux1 | aux2 | aux3 | aux4 | aux5;
+
+assign s[0] = ((~ a[0]) & (b[0])) | ((a[0]) & (~ b[0]));
+
+assign co = (a[1] & b[1]) | (a[0] & b[1] & b[0]) | (a[1] & a[0] & b[0]);
+endmodule
